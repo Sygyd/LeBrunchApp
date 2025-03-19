b05b25a77e4e10506498c7ca1c50c08acd394355
Crysyg <luisjosemartinezc@gmail.com>
2025-03-19 10:08:59 -0400

    0.0.35
    Pruebas de editar

--- lib/Api_services/menu/add_dish_service.dart
+++ lib/Api_services/menu/add_dish_service.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
-class MenuService {
+class AddDishService {
+  // Método para agregar un nuevo plato
   Future<bool> submitDish({
     required String nombre,
     required String categoria,
@@ -11,7 +12,6 @@ class MenuService {
     required File? imagenFile,
   }) async {
     try {
-      // Crear una solicitud multipart
       var request = http.MultipartRequest(
         'POST',
         Uri.parse('http://192.168.1.121:3000/menu'),
@@ -46,6 +46,56 @@ class MenuService {
       }
     } catch (e) {
       throw Exception('Error al agregar plato: $e');
+    }
+  }
+
+  // Método para editar un plato existente
+  Future<bool> updateDish({
+    required String id,
+    required String nombre,
+    required String categoria,
+    required String precio,
+    required String disponibilidad,
+    required String ingredientes,
+    required File? imagenFile,
+  }) async {
+    try {
+      var request = http.MultipartRequest(
+        'PUT', // Usamos PUT para actualizar
+        Uri.parse(
+          'http://192.168.1.121:3000/menu/$id',
+        ), // Incluimos el ID del plato
+      );
+
+      // Agregar campos del formulario
+      request.fields['nombre'] = nombre;
+      request.fields['categoria'] = categoria;
+      request.fields['precio'] = precio;
+      request.fields['disponibilidad'] = disponibilidad;
+      request.fields['ingredientes'] = ingredientes;
+
+      // Agregar la imagen si existe
+      if (imagenFile != null) {
+        var file = await http.MultipartFile.fromPath(
+          'imagen', // Nombre del campo en el backend
+          imagenFile.path, // Ruta del archivo
+        );
+        request.files.add(file);
+      }
+
+      // Enviar la solicitud
+      var response = await request.send();
+
+      // Verificar la respuesta
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        throw Exception(
+          'Error al actualizar plato: ${await response.stream.bytesToString()}',
+        );
+      }
+    } catch (e) {
+      throw Exception('Error al actualizar plato: $e');
     }
   }
 }
--- lib/Api_services/menu/get_dishes_service.dart
+++ lib/Api_services/menu/get_dishes_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class MenuService {
+class GetDishesService {
   Future<List<Map<String, dynamic>>> getDishes() async {
     try {
       final response = await http.get(
